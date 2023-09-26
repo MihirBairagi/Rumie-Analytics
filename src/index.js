4b7f14b53e9c9bffb55f1d0a0231cdde4e94b1a3
MihirBairagi <mihirbairagi10@gmail.com>
2023-09-26 12:58:58 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './assets/css/index.css';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 import App from './pages/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <>
-      <div className="container flex-box">
-        <nav>
-          <img className='w-100' src={require("./assets/img/nav.png")} alt="" />
-        </nav>
-        <App />
-      </div>
-    </>
-  </React.StrictMode>
+    <CssBaseline />
+    <Box sx={{ display: 'flex' }} className='container'>
+      <nav>
+        <img className='w-100' src={require("./assets/img/nav.png")} alt="" />
+      </nav>
+      <App />
+    </Box>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
